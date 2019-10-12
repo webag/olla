f91d0cb254391b32036df9af51e4e8ae193dbe7c
Andrew <andreiduffy@gmail.com>
2019-10-12 17:48:07 +0300

    0.02

--- category-food.php
+++ category-food.php
@@ -2,15 +2,16 @@
 
 <div class="side-line"></div>
 
+
 <section class="s-top-actions">
 	<div class="container top-actions">
 		<a href="categories.php" class="top-actions__back"><i class="i-left"></i>Категории</a>
 	</div>
 </section>
 
+
 <section class="s-categories">
 	<div class="container">
-
 		<div class="cat-grid">
 			<a href="#" class="cat" style="background-image: url(/img/food/pizza.png)">
 				<strong>Пицца</strong>
